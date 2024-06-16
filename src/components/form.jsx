7891945273b12661364459f9336e59a97f30bf0d
flashCrisisEnTierrasInfinitas <desarrollo3@ingeal.com>
2024-06-16 13:53:03 -0500

    cxxc

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -43,8 +43,37 @@ export default function Form() {
     <div className="flex">
       <div className="conter-sms-respo">
         {messages.map((message, index) => (
-          <div key={index} className={`message ${message.sender}`}>
-            <p>{message.text}</p>
+          <div
+            key={index}
+            style={{
+              background: message.color,
+            }}
+          >
+            <label>{message.text}</label>
+            <label>
+              <svg
+                data-slot="icon"
+                fill="none"
+                stroke-width="1.5"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                style={{
+                  width: "20px",
+                  height: "20px",
+                  fontWeight: "900",
+                  color: "green",
+                  float: "right",
+                }}
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                ></path>
+              </svg>
+            </label>
           </div>
         ))}
       </div>
